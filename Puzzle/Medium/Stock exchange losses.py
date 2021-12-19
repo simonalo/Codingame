b9d7fe99a0745f887ca4477e46696e88672be9a9
Aloïs <aloissimon21@gmail.com>
2021-12-19 15:42:40 +0100

    Add medium puzzles

--- Puzzle/Medium/Stock exchange losses.py
+++ Puzzle/Medium/Stock exchange losses.py
@@ -3,11 +3,11 @@ worst = -1
 
 n = int(input())
 for i in input().split():
-    values.append( int(i) )
+    values.append(int(i))
 
-for i in range( len(values) ):
+for i in range(len(values)):
     if worst < values[i]:
-        for j in range ( i+1, len(values) ):
+        for j in range(i + 1, len(values)):
             val = values[i] - values[j]
             if val > worst:
                 worst = val
